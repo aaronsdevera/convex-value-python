55f83991ceab57b4658240f3d599350c785cf035
Aaron DeVera <5042762+aaronsdevera@users.noreply.github.com>
2021-12-23 16:05:51 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='convexvalue',
+setup(
+      name='convexvalue',
       version='1.0',
       description='Python API wrapper library for Convex Value API',
       author='@aaronsdevera',
       author_email='aaronsdevera@protonmail.com',
       url='https://github.com/aaronsdevera/convex-value-python',
-      packages=['convexvalue'],
-      install_requires=['requests','urllib','urllib.parse'],
-     )
+      packages=['convexvalue']
+)
